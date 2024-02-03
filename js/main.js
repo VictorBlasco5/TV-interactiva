f0185b55a90345f4cf901a2801323c0754dd1972
VíctorBlasco5 <victor.blasco.17@gmail.com>
2024-02-03 20:53:25 +0100

    Function for button on off

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 
-function mostrarImagen(imagen) {
-    // Crear un elemento de imagen
+const mostrarImagen = (imagen) => {
+    let encender = document.getElementById("redButton")
+    if(encender.classList.contains("on")) {
+            // Crear un elemento de imagen
     let imgElement = document.createElement("img");
 
     // Establecer la fuente (URL) de la imagen
@@ -16,5 +18,18 @@ function mostrarImagen(imagen) {
 
     // Agregar el elemento de imagen al contenedor div
     document.getElementById("screen").appendChild(imgElement);
+    }
+
   }
 
+  const onOff = () => {
+    let encender = document.getElementById ("redButton");
+    if(encender.classList.contains("on")) {
+        encender.classList.remove("on")
+        encender.classList.add("off")
+        document.getElementById("screen").innerHTML = "";
+    } else {
+        encender.classList.add("on")
+        encender.classList.remove("off")
+    }
+  }
