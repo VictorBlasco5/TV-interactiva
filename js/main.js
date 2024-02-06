6309581c32df3592e3381d78583980f5ac7a0c98
VíctorBlasco5 <victor.blasco.17@gmail.com>
2024-02-06 23:49:03 +0100

    refactor cleanScreen and modify date

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 const mostrarImagen = (imagen) => {
   let pantalla = document.getElementById("screen")
   if (pantalla.classList.contains("on")) {
-    
+
     let imgElement = document.createElement("img");// Crea un elemento de imagen
 
     imgElement.src = "img/" + imagen; // Para establecer la fuente de la imagen
@@ -11,9 +11,10 @@ const mostrarImagen = (imagen) => {
     imgElement.alt = "Loading";
     imgElement.width = 720;
     imgElement.height = 449;
-    imgElement.style.borderRadius = "4px"
-    
-    document.getElementById("screen").innerHTML = "";   // Limpiar el contenido anterior de screen
+    imgElement.style.borderRadius = "0px 0px 4px 4px"
+
+    cleanScreen();
+
     document.getElementById("screen").appendChild(imgElement);   // Agregar la imagen al screen
   }
 }
@@ -26,38 +27,50 @@ const mostrarCifra = (numero) => {
   }
 }
 
- const onOff = () => {
-   let pantalla = document.getElementById("screen");
-    if (pantalla.classList.contains("on")) {  // Compruebo si la pantalla esta encendida
-      pantalla.classList.remove("on") // Quito ON si estuviera encendida
-      pantalla.classList.add("off")     // Añado off para apagarla
-      document.getElementById("screen").innerHTML = "";
-    } else {
-      pantalla.classList.add("on") // Enciendo la pantalla
-      pantalla.classList.remove("off")
-    }
+const onOff = () => {
+  let pantalla = document.getElementById("screen");
+  if (pantalla.classList.contains("on")) {  // Compruebo si la pantalla esta encendida
+    pantalla.classList.remove("on") // Quito ON si estuviera encendida
+    pantalla.classList.add("off")     // Añado off para apagarla
+    fechaYHora.style.display = "none"; // Oculto la fecha cuando la tv está off
+    cleanScreen();
+    let channelDiv = document.getElementById("channel");
+    channelDiv.innerHTML = ""
+  } else {
+    pantalla.classList.add("on"); // Enciendo la pantalla
+    pantalla.classList.remove("off");
+    fechaYHora.style.display = "block"; // Muestro la fecha cuando la tv está on
   }
+}
 
- let actualizarFechaYHora = () => {
-   
-    var fechaYHoraActual = new Date();
-  
-    // Para obtener año, mes, día ,etc
-    var year = fechaYHoraActual.getFullYear();
-    var month = fechaYHoraActual.getMonth() + 1; // Los meses van de 0 a 11, por eso se suma 1
-    var day = fechaYHoraActual.getDate();
-  
-    var hours = fechaYHoraActual.getHours();
-    var minutes = fechaYHoraActual.getMinutes();
-    var seconds = fechaYHoraActual.getSeconds();
-  
-    var fechaYHoraFormateada = day + ' / ' + month + ' / ' + year + ' - ' + hours + ':' + minutes + ':' + seconds;
-   
-    var divFechaYHora = document.getElementById('fechaYHora');  // Obtengo el div por su id
-
-    divFechaYHora.textContent = fechaYHoraFormateada;  // Asigno fecha y hora al contenido del div
+const cleanScreen = () => {
+  let imagenAnterior = document.getElementById("screen").querySelector("img");
+  if(imagenAnterior) {
+    document.getElementById("screen").removeChild(imagenAnterior); 
   }
-  
-  setInterval(actualizarFechaYHora, 1000); // Para actualizar fecha y hora cada segundo (1000 milisegundos)
-  
-  actualizarFechaYHora(); // Llamo a la función al cargar la página para mostrar la fecha y hora
+}
+
+
+let actualizarFechaYHora = () => {
+
+  var fechaYHoraActual = new Date();
+
+  // Para obtener año, mes, día ,etc
+  var year = fechaYHoraActual.getFullYear();
+  var month = fechaYHoraActual.getMonth() + 1; // Los meses van de 0 a 11, por eso se suma 1
+  var day = fechaYHoraActual.getDate();
+
+  var hours = fechaYHoraActual.getHours();
+  var minutes = fechaYHoraActual.getMinutes();
+  var seconds = fechaYHoraActual.getSeconds();
+
+  var fechaYHoraFormateada = day + ' / ' + month + ' / ' + year + ' - ' + hours + ':' + minutes + ':' + seconds;
+
+  var divFechaYHora = document.getElementById('fechaYHora');  // Obtengo el div por su id
+
+  divFechaYHora.textContent = fechaYHoraFormateada;  // Asigno fecha y hora al contenido del div
+}
+
+setInterval(actualizarFechaYHora, 1000); // Para actualizar fecha y hora cada segundo (1000 milisegundos)
+
+actualizarFechaYHora(); // Llamo a la función al cargar la página para mostrar la fecha y hora
